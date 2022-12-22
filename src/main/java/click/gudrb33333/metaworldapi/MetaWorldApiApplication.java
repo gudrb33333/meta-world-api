766af8392811b722c3732f4f6d219b4b075b5d56
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-12-22 21:46:42 +0900

    first commit

---
+++ src/main/java/click/gudrb33333/metaworldapi/MetaWorldApiApplication.java
@@ -0,0 +1,13 @@
+package click.gudrb33333.metaworldapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaWorldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaWorldApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/click/gudrb33333/metaworldapi/MetaWorldApiApplicationTests.java
@@ -0,0 +1,13 @@
+package click.gudrb33333.metaworldapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaWorldApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
