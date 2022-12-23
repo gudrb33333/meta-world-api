9e26317044a838142317c77457995acaaab70a95
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-12-23 21:38:22 +0900

    Create SwaggerConfig.java

---
+++ src/main/java/click/gudrb33333/metaworldapi/configs/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package click.gudrb33333.metaworldapi.configs;
+
+
+import java.util.Collections;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Server;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Value("${ext.appVersion}")
+  private String appVersion;
+
+  private String version;
+
+
+  @Bean
+  public Docket restApiV1() {
+    version = "V1";
+
+    return new Docket(DocumentationType.OAS_30) // open api spec 3.0
+        .apiInfo(apiInfo())
+        .groupName(version)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("click.gudrb33333.metaworldapi.api")
+            .or(RequestHandlerSelectors.basePackage("click.gudrb33333.metaworldapi.api.v1")))
+        .paths(PathSelectors.ant("/*")
+            .or(PathSelectors.ant("/api/v1/**")))
+        .build();
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+        .title("META WORLD REST API")
+        .version(appVersion)
+        .description("")
+        .build();
+  }
+}
