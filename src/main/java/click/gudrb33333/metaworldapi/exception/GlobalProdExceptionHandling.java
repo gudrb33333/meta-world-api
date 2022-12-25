7ffb926ab47e76cc46eb71fe76ee12c4c22958ca
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-12-25 14:04:41 +0900

    Create GlobalProdExceptionHandling.java

---
+++ src/main/java/click/gudrb33333/metaworldapi/exception/GlobalProdExceptionHandling.java
@@ -0,0 +1,77 @@
+package click.gudrb33333.metaworldapi.exception;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.text.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Profile("prod")
+public class GlobalProdExceptionHandling {
+  protected Logger logger;
+
+  public GlobalProdExceptionHandling() {
+    logger = LoggerFactory.getLogger(getClass());
+  }
+
+  public ResponseEntity<?> handleUnAuthenticationException(Exception e) {
+    return errorResponse("400 UNAUTHENTICATED", HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler({
+    DataIntegrityViolationException.class,
+    SQLIntegrityConstraintViolationException.class
+  })
+  public ResponseEntity<?> handleConflictException(Exception e) {
+    return errorResponse("409 CONFLICT", HttpStatus.CONFLICT);
+  }
+
+  @ExceptionHandler({SQLException.class, DataAccessException.class, RuntimeException.class})
+  public ResponseEntity<?> handleSQLException(Exception e) {
+    return errorResponse("500 INTERNAL SERVER ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler({
+    IOException.class,
+    ParseException.class,
+    JsonParseException.class,
+    JsonMappingException.class
+  })
+  public ResponseEntity<?> handleParseException(Exception e) {
+    return errorResponse("400 BAD REQUEST", HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler({InvalidKeyException.class, NoSuchAlgorithmException.class})
+  public ResponseEntity<?> handleHashException(Exception e) {
+    return errorResponse("423 LOCKED", HttpStatus.LOCKED);
+  }
+
+  @ExceptionHandler({Exception.class})
+  public ResponseEntity<?> handleAnyException(Exception e) {
+    return errorResponse("500 INTERNAL SERVER ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler({CatchedException.class})
+  public ResponseEntity<?> handleCatchedException(CatchedException e) {
+    HttpStatus status = e.getStatus();
+    return errorResponse(status.toString(), status);
+  }
+
+  protected ResponseEntity<String> errorResponse(String message, HttpStatus status) {
+    return new ResponseEntity<String>(message, new HttpHeaders(), status);
+  }
+}
