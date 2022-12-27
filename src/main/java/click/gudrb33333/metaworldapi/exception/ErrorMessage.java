44a73bde8d91b9426f2e5df9d67a2c4eb7e98e92
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-12-28 01:28:11 +0900

    Create ErrorMessage.java

---
+++ src/main/java/click/gudrb33333/metaworldapi/exception/ErrorMessage.java
@@ -0,0 +1,7 @@
+package click.gudrb33333.metaworldapi.exception;
+
+public class ErrorMessage {
+    public static final String CONFLICT_EMAIL = "email already exists(unique key violation).";
+
+    public static final String NOT_FOUND_MEMBER = "member not found.";
+}
