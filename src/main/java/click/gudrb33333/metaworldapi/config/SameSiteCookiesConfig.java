266d6d795fadbc43cc548467e73b72ee352c18c5
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-12-27 19:10:47 +0900

    Update to work only in dev profile in SameSiteCookiesConfig.java

--- src/main/java/click/gudrb33333/metaworldapi/config/SameSiteCookiesConfig.java
+++ src/main/java/click/gudrb33333/metaworldapi/config/SameSiteCookiesConfig.java
@@ -5,9 +5,11 @@ import org.apache.tomcat.util.http.SameSiteCookies;
 import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("dev")
 public class SameSiteCookiesConfig implements WebMvcConfigurer {
     @Bean
     public TomcatContextCustomizer setSameSiteCookies() {
