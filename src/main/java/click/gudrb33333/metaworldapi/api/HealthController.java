29bc8d099063121e498107b2e8027a5465bb9e2c
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-12-27 23:10:28 +0900

    Update Api tags value HealthController.java

--- src/main/java/click/gudrb33333/metaworldapi/api/HealthController.java
+++ src/main/java/click/gudrb33333/metaworldapi/api/HealthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
-@Api(tags = {"AWS 로드밸런스 헬스 체크"})
+@Api(tags = {"헬스 체크"})
 public class HealthController {
   @ApiOperation(value = "Health Check", notes="AWS 로드밸런스에서 ec2 서버 헬스체크를 한다.")
   @GetMapping
