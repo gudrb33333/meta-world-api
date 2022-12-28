82bd73f32be02f0999446b0b45ef6c0fc97a8431
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-12-29 00:23:42 +0900

    Fix @Component to @Configuration in CorsConfig.java

--- src/main/java/click/gudrb33333/metaworldapi/config/CorsConfig.java
+++ src/main/java/click/gudrb33333/metaworldapi/config/CorsConfig.java
@@ -2,12 +2,12 @@ package click.gudrb33333.metaworldapi.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Component
+@Configuration
 public class CorsConfig {
 
   @Value("${frontend.domain}")
