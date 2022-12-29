66185838266fe201a078ae6f8e7301c7a034a593
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-12-29 22:52:09 +0900

    Create Avatar.java

---
+++ src/main/java/click/gudrb33333/metaworldapi/entity/Avatar.java
@@ -0,0 +1,43 @@
+package click.gudrb33333.metaworldapi.entity;
+
+import click.gudrb33333.metaworldapi.entity.type.AssetType;
+import click.gudrb33333.metaworldapi.entity.type.AssetType.Values;
+import click.gudrb33333.metaworldapi.entity.type.GenderType;
+import click.gudrb33333.metaworldapi.entity.type.GenderTypeConverter;
+import click.gudrb33333.metaworldapi.entity.type.PublicType;
+import click.gudrb33333.metaworldapi.entity.type.S3DirectoryType;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "avatars")
+@DiscriminatorValue(Values.AVATAR)
+public class Avatar extends Asset {
+
+  @NotNull
+  @Column(name = "gender_type")
+  @Convert(converter = GenderTypeConverter.class)
+  private GenderType genderType;
+
+  @Builder
+  public Avatar(
+      AssetType assetType,
+      UUID s3AssetUUID,
+      String extension,
+      S3DirectoryType s3DirectoryType,
+      PublicType publicType,
+      GenderType genderType) {
+    super(assetType, s3AssetUUID, extension, s3DirectoryType, publicType);
+    this.genderType = genderType;
+  }
+}
