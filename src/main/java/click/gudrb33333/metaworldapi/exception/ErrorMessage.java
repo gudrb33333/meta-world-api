7e665e4d6b4d6af09b3bab30fe280feeb4fab5a2
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-12-29 23:13:24 +0900

    Add NOT_FOUND_AVATAR and AWS_S3_UTIL_IO_ERROR to ErrorMessage.java

--- src/main/java/click/gudrb33333/metaworldapi/exception/ErrorMessage.java
+++ src/main/java/click/gudrb33333/metaworldapi/exception/ErrorMessage.java
@@ -3,5 +3,10 @@ package click.gudrb33333.metaworldapi.exception;
 public class ErrorMessage {
     public static final String CONFLICT_EMAIL = "email already exists(unique key violation).";
 
+    /// NOT FOUND
     public static final String NOT_FOUND_MEMBER = "member not found.";
+    public static final String NOT_FOUND_AVATAR = "avatar not found.";
+
+    // INTERNAL_SERVER_ERROR
+    public static final String AWS_S3_UTIL_IO_ERROR = "IO error in AwsS3Util.";
 }
