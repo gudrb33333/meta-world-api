fd990a41fbba9de1ff98af1280c830a9002f57d8
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-12-29 23:07:33 +0900

    Create AvatarCreateDto.java

---
+++ src/main/java/click/gudrb33333/metaworldapi/api/v1/avatar/dto/AvatarCreateDto.java
@@ -0,0 +1,28 @@
+package click.gudrb33333.metaworldapi.api.v1.avatar.dto;
+
+import click.gudrb33333.metaworldapi.entity.type.GenderType;
+import click.gudrb33333.metaworldapi.entity.type.GenderTypeConverter;
+import click.gudrb33333.metaworldapi.entity.type.PublicType;
+import io.swagger.annotations.ApiModelProperty;
+import javax.persistence.Convert;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AvatarCreateDto {
+
+  @ApiModelProperty(position = 1, value = "성별", allowableValues = "male,female")
+  private GenderType genderType;
+
+  @ApiModelProperty(position = 2, value = "공개 여부", allowableValues = "public,private")
+  private PublicType publicType;
+
+  @Builder
+  public AvatarCreateDto(GenderType genderType, PublicType publicType) {
+    this.genderType = genderType;
+    this.publicType = publicType;
+  }
+}
