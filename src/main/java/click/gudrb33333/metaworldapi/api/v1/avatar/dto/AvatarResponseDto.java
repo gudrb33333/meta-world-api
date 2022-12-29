0d3a100b2339d48257b92ea09c9c25108b3a6c5b
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2022-12-29 23:07:41 +0900

    Create AvatarResponseDto.java

---
+++ src/main/java/click/gudrb33333/metaworldapi/api/v1/avatar/dto/AvatarResponseDto.java
@@ -0,0 +1,42 @@
+package click.gudrb33333.metaworldapi.api.v1.avatar.dto;
+
+import click.gudrb33333.metaworldapi.entity.type.AssetType;
+import click.gudrb33333.metaworldapi.entity.type.GenderType;
+import click.gudrb33333.metaworldapi.entity.type.PublicType;
+import click.gudrb33333.metaworldapi.entity.type.S3DirectoryType;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AvatarResponseDto {
+
+  private UUID assetId;
+  private AssetType assetType;
+  private UUID s3AssetUUID;
+  private String extension;
+  private S3DirectoryType s3DirectoryType;
+  private PublicType publicTyp;
+  private GenderType genderType;
+
+  @Builder
+  public AvatarResponseDto(
+      UUID assetId,
+      AssetType assetType,
+      UUID s3AssetUUID,
+      String extension,
+      S3DirectoryType s3DirectoryType,
+      PublicType publicType,
+      GenderType genderType) {
+    this.assetId = assetId;
+    this.assetType = assetType;
+    this.s3AssetUUID = s3AssetUUID;
+    this.extension = extension;
+    this.s3DirectoryType = s3DirectoryType;
+    this.publicTyp = publicType;
+    this.genderType = genderType;
+  }
+}
