e12bbb61e2cc20ba3266016a790152c35676d664
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-01 01:21:12 +0900

    Add some message to ErrorMessage.java

--- src/main/java/click/gudrb33333/metaworldapi/exception/ErrorMessage.java
+++ src/main/java/click/gudrb33333/metaworldapi/exception/ErrorMessage.java
@@ -1,12 +1,16 @@
 package click.gudrb33333.metaworldapi.exception;
 
 public class ErrorMessage {
-    public static final String CONFLICT_EMAIL = "email already exists(unique key violation).";
 
-    /// NOT FOUND
-    public static final String NOT_FOUND_MEMBER = "member not found.";
-    public static final String NOT_FOUND_AVATAR = "avatar not found.";
+  // CONFLICT
+  public static final String CONFLICT_EMAIL = "email already exists(unique key violation).";
+  public static final String CONFLICT_PROFILE = "profile already exists.";
 
-    // INTERNAL_SERVER_ERROR
-    public static final String AWS_S3_UTIL_IO_ERROR = "IO error in AwsS3Util.";
+  /// NOT FOUND
+  public static final String NOT_FOUND_MEMBER = "member not found.";
+  public static final String NOT_FOUND_AVATAR = "avatar not found.";
+  public static final String NOT_FOUND_PROFILE = "profile not found.";
+
+  // INTERNAL_SERVER_ERROR
+  public static final String AWS_S3_UTIL_IO_ERROR = "IO error in AwsS3Util.";
 }
