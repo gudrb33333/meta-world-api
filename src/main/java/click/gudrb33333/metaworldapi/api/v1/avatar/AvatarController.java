4e090e5dfd9ac36697a01c1ead2aaafa9c8843ec
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-01 00:49:38 +0900

    Fix to receive RequestBody as a parameter in the create method in AvatarController.java

--- src/main/java/click/gudrb33333/metaworldapi/api/v1/avatar/AvatarController.java
+++ src/main/java/click/gudrb33333/metaworldapi/api/v1/avatar/AvatarController.java
@@ -3,6 +3,7 @@ package click.gudrb33333.metaworldapi.api.v1.avatar;
 import click.gudrb33333.metaworldapi.api.v1.avatar.dto.AvatarCreateDto;
 import click.gudrb33333.metaworldapi.api.v1.avatar.dto.AvatarResponseDto;
 import io.swagger.annotations.Api;
+import java.io.IOException;
 import java.util.UUID;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,10 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @Api(tags = {"아바타 API"})
 @RequestMapping("/api/v1/avatars")
@@ -26,9 +26,8 @@ public class AvatarController {
 
   @PostMapping
   public ResponseEntity<String> create(
-      @Valid @RequestPart AvatarCreateDto avatarCreateDto,
-      @RequestPart("avatarFile") MultipartFile avatarFile) {
-    avatarService.createAvatar(avatarCreateDto, avatarFile);
+      @Valid @RequestBody AvatarCreateDto avatarCreateDto) throws IOException {
+    avatarService.createAvatar(avatarCreateDto);
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 
