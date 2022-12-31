5fb3d95b39a0810cce856001f852c3fd38292ccd
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-01 01:16:17 +0900

    Create AvatarRepositoryImpl.java and override findOneMemberAvatar method

---
+++ src/main/java/click/gudrb33333/metaworldapi/repository/AvatarRepositoryImpl.java
@@ -0,0 +1,32 @@
+package click.gudrb33333.metaworldapi.repository;
+
+import static click.gudrb33333.metaworldapi.entity.QAsset.asset;
+import static click.gudrb33333.metaworldapi.entity.QMemberAsset.memberAsset;
+
+import click.gudrb33333.metaworldapi.entity.Avatar;
+import click.gudrb33333.metaworldapi.entity.Member;
+import click.gudrb33333.metaworldapi.entity.MemberAsset;
+import click.gudrb33333.metaworldapi.entity.type.AssetType;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class AvatarRepositoryImpl implements AvatarRepositoryCustom {
+
+  private final JPAQueryFactory queryFactory;
+
+  @Override
+  public Optional<Avatar> findOneMemberAvatar(Member member) {
+
+    return Optional.ofNullable(
+            queryFactory
+                .selectFrom(memberAsset)
+                .innerJoin(memberAsset.asset, asset)
+                .fetchJoin()
+                .where(asset.assetType.eq(AssetType.AVATAR).and(memberAsset.member.eq(member)))
+                .orderBy(memberAsset.insertedAt.desc())
+                .fetchFirst())
+        .map(result -> (Avatar) result.getAsset());
+  }
+}
