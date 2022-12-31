0618e718324c879c9b5eb50017bd12a954448127
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-01 01:21:49 +0900

    Add profile association to Member.java

--- src/main/java/click/gudrb33333/metaworldapi/entity/Member.java
+++ src/main/java/click/gudrb33333/metaworldapi/entity/Member.java
@@ -10,8 +10,11 @@ import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -28,42 +31,51 @@ import org.hibernate.annotations.UpdateTimestamp;
 @Table(name = "members")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+
   @Id
   @GeneratedValue(generator = "UUID")
-  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-  @Column(name = "member_id", columnDefinition = "char(36)")
   @Type(type = "org.hibernate.type.UUIDCharType")
+  @Column(name = "member_id", columnDefinition = "char(36)")
+  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
   private UUID id;
 
-  @Column(unique = true)
   @NotNull
+  @Column(unique = true)
   private String email;
 
-  @NotNull
-  private String password;
+  @NotNull private String password;
 
   @NotNull
   @Column(name = "login_type")
   @Convert(converter = LoginTypeConverter.class)
   private LoginType loginType;
 
-  @Enumerated(EnumType.STRING)
   @NotNull
+  @Enumerated(EnumType.STRING)
   private Role role;
 
-  @Column(name = "inserted_at")
   @CreationTimestamp
+  @Column(name = "inserted_at")
   private LocalDateTime insertedAt;
 
-  @Column(name = "updated_at")
   @UpdateTimestamp
+  @Column(name = "updated_at")
   private LocalDateTime updatedAt;
 
+  @JoinColumn(name = "profile_id")
+  @OneToOne(targetEntity = Profile.class, fetch = FetchType.LAZY)
+  private Profile profile;
+
   @Builder
-  public Member(String email, String password, LoginType loginType, Role role) {
+  public Member(String email, String password, LoginType loginType, Role role, Profile profile) {
     this.email = email;
     this.password = password;
     this.loginType = loginType;
     this.role = role;
+    this.profile = profile;
+  }
+
+  public void changeProfile(Profile profile){
+    this.profile = profile;
   }
 }
