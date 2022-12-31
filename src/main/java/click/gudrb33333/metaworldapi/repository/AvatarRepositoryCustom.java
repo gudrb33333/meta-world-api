c05480bebed7483ca245c0343c8a8c40a895aad7
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-01 01:15:40 +0900

    Create AvatarRepositoryCustom.java and add findOneMemberAvatar method

---
+++ src/main/java/click/gudrb33333/metaworldapi/repository/AvatarRepositoryCustom.java
@@ -0,0 +1,9 @@
+package click.gudrb33333.metaworldapi.repository;
+
+import click.gudrb33333.metaworldapi.entity.Avatar;
+import click.gudrb33333.metaworldapi.entity.Member;
+import java.util.Optional;
+
+public interface AvatarRepositoryCustom {
+  Optional<Avatar> findOneMemberAvatar(Member member);
+}
