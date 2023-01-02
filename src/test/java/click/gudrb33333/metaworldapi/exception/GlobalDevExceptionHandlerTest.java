5b2376d11bf3a163506d145c7464bc30a4ff3081
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-03 06:31:35 +0900

    Create GlobalDevExceptionHandlerTest.java

---
+++ src/test/java/click/gudrb33333/metaworldapi/exception/GlobalDevExceptionHandlerTest.java
@@ -0,0 +1,107 @@
+package click.gudrb33333.metaworldapi.exception;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.rest.webmvc.support.ExceptionMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@ExtendWith(MockitoExtension.class)
+class GlobalDevExceptionHandlerTest {
+
+  @InjectMocks GlobalDevExceptionHandler globalDevExceptionHandler;
+
+  private final Exception testException = new Exception();
+
+  @Test
+  void handleUnAuthenticationException() {
+    ResponseEntity<ExceptionMessage> response =
+        (ResponseEntity<ExceptionMessage>)
+            globalDevExceptionHandler.handleUnAuthenticationException(testException);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    assertThat(response.getStatusCodeValue()).isEqualTo(400);
+  }
+
+  @Test
+  void handleConflictException() {
+    ResponseEntity<ExceptionMessage> response =
+        (ResponseEntity<ExceptionMessage>)
+            globalDevExceptionHandler.handleConflictException(testException);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
+    assertThat(response.getStatusCodeValue()).isEqualTo(409);
+  }
+
+  @Test
+  void handleSQLException() {
+    ResponseEntity<ExceptionMessage> response =
+        (ResponseEntity<ExceptionMessage>)
+            globalDevExceptionHandler.handleSQLException(testException);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    assertThat(response.getStatusCodeValue()).isEqualTo(500);
+  }
+
+  @Test
+  void handleParseException() {
+    ResponseEntity<ExceptionMessage> response =
+        (ResponseEntity<ExceptionMessage>)
+            globalDevExceptionHandler.handleParseException(testException);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    assertThat(response.getStatusCodeValue()).isEqualTo(400);
+  }
+
+  @Test
+  void handleHashException() {
+    ResponseEntity<ExceptionMessage> response =
+        (ResponseEntity<ExceptionMessage>)
+            globalDevExceptionHandler.handleHashException(testException);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.LOCKED);
+    assertThat(response.getStatusCodeValue()).isEqualTo(423);
+  }
+
+  @Test
+  void handleAnyException() {
+    ResponseEntity<ExceptionMessage> response =
+        (ResponseEntity<ExceptionMessage>)
+            globalDevExceptionHandler.handleAnyException(testException);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    assertThat(response.getStatusCodeValue()).isEqualTo(500);
+  }
+
+  @Test
+  void handleCatchedException() {
+    CatchedException testCatchedException =
+        new CatchedException(ErrorMessage.NOT_FOUND_PROFILE, HttpStatus.NOT_FOUND);
+
+    ResponseEntity<ExceptionMessage> response =
+        (ResponseEntity<ExceptionMessage>)
+            globalDevExceptionHandler.handleCatchedException(testCatchedException);
+
+    assertThat(response.getStatusCode()).isEqualTo(testCatchedException.getStatus());
+    assertThat(response.getStatusCodeValue()).isEqualTo(404);
+  }
+
+  @Test
+  void errorResponse() {
+    HttpStatus testStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    Throwable testThrowable = null;
+
+    ResponseEntity<ExceptionMessage> response =
+        globalDevExceptionHandler.errorResponse(testThrowable, testStatus);
+
+    assertThat(response.getBody()).isEqualTo(null);
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    assertThat(response.getStatusCodeValue()).isEqualTo(500);
+  }
+}
