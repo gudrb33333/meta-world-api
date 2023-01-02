5607cb112d4bd9d40bfdcade7d25912a692852ce
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-03 06:31:30 +0900

    Create AssetTypeTest.java

---
+++ src/test/java/click/gudrb33333/metaworldapi/entity/type/AssetTypeTest.java
@@ -0,0 +1,14 @@
+package click.gudrb33333.metaworldapi.entity.type;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import click.gudrb33333.metaworldapi.entity.type.AssetType.Values;
+import org.junit.jupiter.api.Test;
+
+class AssetTypeTest {
+
+    @Test
+    void values() {
+    }
+}
