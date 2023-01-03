525aecc3a46a8e8d8286b8e5437ec72020cc45c2
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-04 07:26:11 +0900

    Add findOne method to ClothingController.java

--- src/main/java/click/gudrb33333/metaworldapi/api/v1/clothing/ClothingController.java
+++ src/main/java/click/gudrb33333/metaworldapi/api/v1/clothing/ClothingController.java
@@ -1,13 +1,20 @@
 package click.gudrb33333.metaworldapi.api.v1.clothing;
 
 import click.gudrb33333.metaworldapi.api.v1.clothing.dto.ClothingCreateDto;
+import click.gudrb33333.metaworldapi.api.v1.clothing.dto.ClothingResponseDto;
 import click.gudrb33333.metaworldapi.entity.Member;
 import click.gudrb33333.metaworldapi.util.SessionUtil;
 import io.swagger.annotations.Api;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.UUID;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.jets3t.service.CloudFrontServiceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -30,5 +37,12 @@ public class ClothingController {
     Member sessionMember = sessionUtil.getCurrentMember();
     clothingService.createClothing(clothingCreateDto, multipartFile, sessionMember);
     return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  @GetMapping("/{uuid}")
+  public ResponseEntity<ClothingResponseDto> findOne(@PathVariable("uuid") UUID uuid)
+      throws IOException, ParseException, CloudFrontServiceException {
+    Member sessionMember = sessionUtil.getCurrentMember();
+    return ResponseEntity.ok().body(clothingService.findOneClothing(uuid, sessionMember));
   }
 }
