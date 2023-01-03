d3f69ef94f1e33d278a93ed27c8d2f0fcc397ec9
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-04 00:44:16 +0900

    Create ClothingRepository.java

---
+++ src/main/java/click/gudrb33333/metaworldapi/repository/ClothingRepository.java
@@ -0,0 +1,9 @@
+package click.gudrb33333.metaworldapi.repository;
+
+import click.gudrb33333.metaworldapi.entity.Clothing;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClothingRepository extends JpaRepository<Clothing, UUID> {}
