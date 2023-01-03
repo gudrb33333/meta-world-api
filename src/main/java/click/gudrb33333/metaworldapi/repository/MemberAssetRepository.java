26bf3b4f0b3a494443ec573020a239db99964ebd
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-04 07:24:53 +0900

    Add findByMemberAndAsset method to MemberAssetRepository.java

--- src/main/java/click/gudrb33333/metaworldapi/repository/MemberAssetRepository.java
+++ src/main/java/click/gudrb33333/metaworldapi/repository/MemberAssetRepository.java
@@ -1,9 +1,15 @@
 package click.gudrb33333.metaworldapi.repository;
 
+import click.gudrb33333.metaworldapi.entity.Clothing;
+import click.gudrb33333.metaworldapi.entity.Member;
 import click.gudrb33333.metaworldapi.entity.MemberAsset;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberAssetRepository extends JpaRepository<MemberAsset, UUID> {}
+public interface MemberAssetRepository extends JpaRepository<MemberAsset, UUID> {
+
+  Optional<MemberAsset> findByMemberAndAsset(Member sessionMember, Clothing clothing);
+}
