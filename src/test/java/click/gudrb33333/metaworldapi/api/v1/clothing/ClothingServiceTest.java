0aa2e013afe095ac928528a469af474d29bd0535
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-04 08:46:56 +0900

    Add findOneClothing method test to ClothingServiceTest.java

--- src/test/java/click/gudrb33333/metaworldapi/api/v1/clothing/ClothingServiceTest.java
+++ src/test/java/click/gudrb33333/metaworldapi/api/v1/clothing/ClothingServiceTest.java
@@ -1,10 +1,12 @@
 package click.gudrb33333.metaworldapi.api.v1.clothing;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 import click.gudrb33333.metaworldapi.api.v1.clothing.dto.ClothingCreateDto;
+import click.gudrb33333.metaworldapi.api.v1.clothing.dto.ClothingResponseDto;
 import click.gudrb33333.metaworldapi.entity.Clothing;
 import click.gudrb33333.metaworldapi.entity.Member;
 import click.gudrb33333.metaworldapi.entity.MemberAsset;
@@ -13,15 +15,23 @@ import click.gudrb33333.metaworldapi.entity.type.ExtensionType;
 import click.gudrb33333.metaworldapi.entity.type.GenderType;
 import click.gudrb33333.metaworldapi.entity.type.PublicType;
 import click.gudrb33333.metaworldapi.entity.type.S3DirectoryType;
+import click.gudrb33333.metaworldapi.exception.CatchedException;
+import click.gudrb33333.metaworldapi.exception.ErrorMessage;
 import click.gudrb33333.metaworldapi.repository.ClothingRepository;
 import click.gudrb33333.metaworldapi.repository.MemberAssetRepository;
 import click.gudrb33333.metaworldapi.util.AwsS3Util;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Optional;
 import java.util.UUID;
+import org.jets3t.service.CloudFrontServiceException;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockMultipartFile;
 
 @ExtendWith(MockitoExtension.class)
@@ -87,5 +97,106 @@ class ClothingServiceTest {
 
     assertThat(saveTestMemberAsset.getMember()).isEqualTo(testMember);
     assertThat(saveTestMemberAsset.getAsset()).isEqualTo(savedTestClothing);
+  }
+
+  @Nested
+  class findOneClothing {
+
+    Member testMember = Member.builder().email("test@test.com").build();
+    UUID testUUID = UUID.randomUUID();
+
+    @Test
+    void whenClothingNotFound() {
+      given(clothingRepository.findById(testUUID)).willReturn(Optional.empty());
+
+      assertThatThrownBy(
+              () -> {
+                clothingService.findOneClothing(testUUID, testMember);
+              })
+          .isInstanceOf(CatchedException.class)
+          .hasMessageContaining(ErrorMessage.NOT_FOUND_CLOTHING);
+    }
+
+    @Nested
+    class whenClothingExistAndPublicTypeIsPrivate {
+
+      Clothing testClothing =
+          Clothing.builder()
+              .id(testUUID)
+              .publicType(PublicType.PRIVATE)
+              .brand("testBrand")
+              .detailDescription("testDetailDescription")
+              .name("testName")
+              .price(1000)
+              .serialNumber("testSerialNumber")
+              .associateLink("testAssociateLink")
+              .build();
+
+      @Test
+      void andMemberAssetNotFound() {
+        given(clothingRepository.findById(testUUID)).willReturn(Optional.of(testClothing));
+
+        given(memberAssetRepository.findByMemberAndAsset(testMember, testClothing))
+            .willReturn(Optional.empty());
+
+        assertThatThrownBy(
+                () -> {
+                  clothingService.findOneClothing(testUUID, testMember);
+                })
+            .isInstanceOf(CatchedException.class)
+            .hasMessageContaining(HttpStatus.FORBIDDEN.toString());
+      }
+
+      @Test
+      void andMemberAssetExist() throws IOException, ParseException, CloudFrontServiceException {
+        MemberAsset testMemberAsset =
+            MemberAsset.builder()
+                .id(UUID.randomUUID())
+                .member(testMember)
+                .asset(testClothing)
+                .build();
+
+        given(clothingRepository.findById(testUUID)).willReturn(Optional.of(testClothing));
+
+        given(memberAssetRepository.findByMemberAndAsset(testMember, testClothing))
+            .willReturn(Optional.of(testMemberAsset));
+
+        given(awsS3Util.createSignedUrl(S3DirectoryType.CLOTHING, testUUID))
+            .willReturn("testSignedUrl");
+
+        ClothingResponseDto clothingResponseDto =
+            clothingService.findOneClothing(testUUID, testMember);
+
+        assertThat(clothingResponseDto.getId()).isEqualTo(testClothing.getId());
+        assertThat(clothingResponseDto.getSignedClothingUrl()).isEqualTo("testSignedUrl");
+      }
+    }
+
+    @Test
+    void whenClothingExistAndPublicTypeIsPublic()
+        throws IOException, ParseException, CloudFrontServiceException {
+      Clothing testClothing =
+          Clothing.builder()
+              .id(testUUID)
+              .publicType(PublicType.PUBLIC)
+              .brand("testBrand")
+              .detailDescription("testDetailDescription")
+              .name("testName")
+              .price(1000)
+              .serialNumber("testSerialNumber")
+              .associateLink("testAssociateLink")
+              .build();
+
+      given(clothingRepository.findById(testUUID)).willReturn(Optional.of(testClothing));
+
+      given(awsS3Util.createSignedUrl(S3DirectoryType.CLOTHING, testUUID))
+          .willReturn("testSignedUrl");
+
+      ClothingResponseDto clothingResponseDto =
+          clothingService.findOneClothing(testUUID, testMember);
+
+      assertThat(clothingResponseDto.getId()).isEqualTo(testClothing.getId());
+      assertThat(clothingResponseDto.getSignedClothingUrl()).isEqualTo("testSignedUrl");
+    }
   }
 }
