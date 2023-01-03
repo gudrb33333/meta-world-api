415cbcb770f4ab57ab4a255be29454730f53a1ba
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-04 00:44:11 +0900

    Add Builder logic to Clothing.java

--- src/main/java/click/gudrb33333/metaworldapi/entity/Clothing.java
+++ src/main/java/click/gudrb33333/metaworldapi/entity/Clothing.java
@@ -1,14 +1,20 @@
 package click.gudrb33333.metaworldapi.entity;
 
+import click.gudrb33333.metaworldapi.entity.type.AssetType;
 import click.gudrb33333.metaworldapi.entity.type.AssetType.Values;
+import click.gudrb33333.metaworldapi.entity.type.ExtensionType;
 import click.gudrb33333.metaworldapi.entity.type.GenderType;
 import click.gudrb33333.metaworldapi.entity.type.GenderTypeConverter;
+import click.gudrb33333.metaworldapi.entity.type.PublicType;
+import click.gudrb33333.metaworldapi.entity.type.S3DirectoryType;
+import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,24 +23,50 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Table(name = "clothing")
 @DiscriminatorValue(Values.CLOTHING)
-public class Clothing extends Asset{
+public class Clothing extends Asset {
 
-    private String name;
+  private String name;
 
-    private String brand;
+  private String brand;
 
-    @Column(name = "serial_number")
-    private String serialNumber;
+  @Column(name = "serial_number")
+  private String serialNumber;
 
-    @Column(name = "gender_type")
-    @Convert(converter = GenderTypeConverter.class)
-    private GenderType genderType;
+  @Column(name = "gender_type")
+  @Convert(converter = GenderTypeConverter.class)
+  private GenderType genderType;
 
-    private int price;
+  private int price;
 
-    @Column(name = "associate_link")
-    private String associateLink;
+  @Column(name = "associate_link")
+  private String associateLink;
 
-    @Column(name = "detail_description", columnDefinition = "TEXT")
-    private String detailDescription;
+  @Column(name = "detail_description", columnDefinition = "TEXT")
+  private String detailDescription;
+
+  @Builder
+  public Clothing(
+      UUID id,
+      AssetType assetType,
+      UUID s3AssetUUID,
+      ExtensionType extension,
+      S3DirectoryType s3DirectoryType,
+      PublicType publicType,
+      String name,
+      String brand,
+      String serialNumber,
+      GenderType genderType,
+      int price,
+      String associateLink,
+      String detailDescription
+      ) {
+    super(id, assetType, s3AssetUUID, extension, s3DirectoryType, publicType);
+    this.name = name;
+    this.brand = brand;
+    this.serialNumber = serialNumber;
+    this.genderType = genderType;
+    this.price = price;
+    this.associateLink = associateLink;
+    this.detailDescription = detailDescription;
+  }
 }
