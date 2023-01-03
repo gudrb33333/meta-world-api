cab453fd2a927b5e6ac9cfdb908a8b09e25f4407
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-04 04:05:40 +0900

    Create ClothingServiceTest.java

---
+++ src/test/java/click/gudrb33333/metaworldapi/api/v1/clothing/ClothingServiceTest.java
@@ -0,0 +1,11 @@
+package click.gudrb33333.metaworldapi.api.v1.clothing;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ClothingServiceTest {
+
+  @InjectMocks ClothingService clothingService;
+}
