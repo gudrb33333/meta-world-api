9470b565ce1f5e02e23d96c63632e05a978fa847
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-04 00:42:44 +0900

    Create ClothingService.java

---
+++ src/main/java/click/gudrb33333/metaworldapi/api/v1/clothing/ClothingService.java
@@ -0,0 +1,8 @@
+package click.gudrb33333.metaworldapi.api.v1.clothing;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ClothingService {}
