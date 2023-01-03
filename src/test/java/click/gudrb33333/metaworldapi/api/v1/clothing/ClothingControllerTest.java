515e3d528b4b5ab6a18f06412418964d016e9710
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-04 08:47:12 +0900

    Add findOne method test to ClothingControllerTest.java

--- src/test/java/click/gudrb33333/metaworldapi/api/v1/clothing/ClothingControllerTest.java
+++ src/test/java/click/gudrb33333/metaworldapi/api/v1/clothing/ClothingControllerTest.java
@@ -1,10 +1,16 @@
 package click.gudrb33333.metaworldapi.api.v1.clothing;
 
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import click.gudrb33333.metaworldapi.api.v1.clothing.dto.ClothingCreateDto;
+import click.gudrb33333.metaworldapi.api.v1.clothing.dto.ClothingResponseDto;
 import click.gudrb33333.metaworldapi.entity.Member;
 import click.gudrb33333.metaworldapi.entity.type.GenderType;
 import click.gudrb33333.metaworldapi.entity.type.PublicType;
@@ -12,12 +18,14 @@ import click.gudrb33333.metaworldapi.entity.type.Role;
 import click.gudrb33333.metaworldapi.support.WithAuthMember;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
@@ -80,5 +88,30 @@ class ClothingControllerTest {
     mockMvc
         .perform(multipart("/api/v1/clothing").file(testFile).file(jsonFile))
         .andExpect(status().isCreated());
+  }
+
+  @Test
+  @WithAuthMember(email = "test@test.com", role = Role.MEMBER)
+  void findOne() throws Exception {
+    Member testMember = (Member) mockHttpSession.getAttribute("current-member");
+    UUID testAssetId = UUID.randomUUID();
+    ClothingResponseDto testClothingResponseDto =
+        ClothingResponseDto.builder()
+            .id(testAssetId)
+            .signedClothingUrl("testSignedUrl")
+            .associateLink("testAssociateLink")
+            .serialNumber("testSerialNumber")
+            .price(10000)
+            .name("testName")
+            .detailDescription("testDetailDescription")
+            .brand("testBrand")
+            .build();
+
+    given(clothingService.findOneClothing(testAssetId, testMember))
+        .willReturn(testClothingResponseDto);
+
+    mockMvc
+        .perform(get("/api/v1/clothing/" + testAssetId))
+        .andExpect(status().isOk());
   }
 }
