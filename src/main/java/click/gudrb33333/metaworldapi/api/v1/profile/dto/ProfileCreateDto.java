6bcbdca2390e752fc5156895cbce11a774c56318
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-07 11:15:19 +0900

    Add fields for avatar creation to ProfileCreateDto.java

--- src/main/java/click/gudrb33333/metaworldapi/api/v1/profile/dto/ProfileCreateDto.java
+++ src/main/java/click/gudrb33333/metaworldapi/api/v1/profile/dto/ProfileCreateDto.java
@@ -1,5 +1,9 @@
 package click.gudrb33333.metaworldapi.api.v1.profile.dto;
 
+import click.gudrb33333.metaworldapi.entity.type.GenderType;
+import click.gudrb33333.metaworldapi.entity.type.PublicType;
+import io.swagger.annotations.ApiModelProperty;
+import javax.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,10 +13,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProfileCreateDto {
 
-    private String nickname;
+  @NotBlank
+  @ApiModelProperty(value = "닉네임", required = true)
+  private String nickname;
 
-    @Builder
-    public ProfileCreateDto(String nickname) {
-        this.nickname = nickname;
-    }
+  @ApiModelProperty(value = "성별", allowableValues = "male,female")
+  private GenderType genderType;
+
+  @ApiModelProperty(value = "공개 여부", allowableValues = "public,private", required = true)
+  private PublicType publicType;
+
+  @ApiModelProperty(value = "Ready Player Me 아바타 url", required = true)
+  private String avatarUrl;
+
+  @Builder
+  public ProfileCreateDto(
+      String nickname, GenderType genderType, PublicType publicType, String avatarUrl) {
+    this.nickname = nickname;
+    this.genderType = genderType;
+    this.publicType = publicType;
+    this.avatarUrl = avatarUrl;
+  }
 }
