64c727285b31517ab00748a50a8c1593b88776c5
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-07 09:45:05 +0900

    Fix logic to fetch members and profiles using findMemberWithProfile method in ProfileService.java

--- src/main/java/click/gudrb33333/metaworldapi/api/v1/profile/ProfileService.java
+++ src/main/java/click/gudrb33333/metaworldapi/api/v1/profile/ProfileService.java
@@ -10,26 +10,15 @@ import click.gudrb33333.metaworldapi.entity.type.S3DirectoryType;
 import click.gudrb33333.metaworldapi.exception.CatchedException;
 import click.gudrb33333.metaworldapi.exception.ErrorMessage;
 import click.gudrb33333.metaworldapi.repository.AvatarRepository;
-import click.gudrb33333.metaworldapi.repository.MemberAssetRepository;
 import click.gudrb33333.metaworldapi.repository.MemberRepository;
 import click.gudrb33333.metaworldapi.repository.ProfileRepository;
 import click.gudrb33333.metaworldapi.util.AwsS3Util;
-import click.gudrb33333.metaworldapi.util.SessionUtil;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.io.IOException;
-import java.io.InputStream;
 import java.text.ParseException;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.jets3t.service.CloudFrontServiceException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -65,44 +54,36 @@ public class ProfileService {
 
   public ProfileResponseDto findSigninMemberProfile(Member member)
       throws IOException, ParseException, CloudFrontServiceException {
-    Profile memberProfile = member.getProfile();
-
-    if (memberProfile == null) {
-      throw new CatchedException(ErrorMessage.NOT_FOUND_PROFILE, HttpStatus.NOT_FOUND);
-    }
-
-    memberProfile =
-        profileRepository
-            .findById(memberProfile.getId())
+    Member memberWithProfile =
+        memberRepository
+            .findMemberWithProfile(member)
             .orElseThrow(
                 () -> {
                   throw new CatchedException(ErrorMessage.NOT_FOUND_PROFILE, HttpStatus.NOT_FOUND);
                 });
 
+    Profile profile = memberWithProfile.getProfile();
+
     String signedAvatarUrl =
         awsS3Util.createSignedUrl(
-            S3DirectoryType.AVATAR, memberProfile.getAvatar().getS3AssetUUID(), 3600);
+            S3DirectoryType.AVATAR, profile.getAvatar().getS3AssetUUID(), 3600);
 
     return ProfileResponseDto.builder()
-        .nickname(memberProfile.getNickname())
+        .nickname(profile.getNickname())
         .signedAvatarUrl(signedAvatarUrl)
         .build();
   }
 
   public void updateSigninMemberProfile(ProfileUpdateDto profileUpdateDto, Member member) {
-    Profile memberProfile = member.getProfile();
-
-    if (memberProfile == null) {
-      throw new CatchedException(ErrorMessage.NOT_FOUND_PROFILE, HttpStatus.NOT_FOUND);
-    }
-
-    memberProfile =
-        profileRepository
-            .findById(memberProfile.getId())
+    Member memberWithProfile =
+        memberRepository
+            .findMemberWithProfile(member)
             .orElseThrow(
                 () -> {
                   throw new CatchedException(ErrorMessage.NOT_FOUND_PROFILE, HttpStatus.NOT_FOUND);
                 });
+
+    Profile profile = memberWithProfile.getProfile();
 
     Avatar avatar =
         avatarRepository
@@ -112,7 +93,7 @@ public class ProfileService {
                   throw new CatchedException(ErrorMessage.NOT_FOUND_AVATAR, HttpStatus.NOT_FOUND);
                 });
 
-    memberProfile.changeAvatarAndNickname(avatar, profileUpdateDto.getNickname());
-    profileRepository.save(memberProfile);
+    profile.changeAvatarAndNickname(avatar, profileUpdateDto.getNickname());
+    profileRepository.save(profile);
   }
 }
