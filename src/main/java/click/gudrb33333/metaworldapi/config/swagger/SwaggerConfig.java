859fffa5b31a1e7c3d4ec19793486f39459fedcd
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-08 10:31:05 +0900

    Remove unnecessary options from SwaggerConfig.java

--- src/main/java/click/gudrb33333/metaworldapi/config/swagger/SwaggerConfig.java
+++ src/main/java/click/gudrb33333/metaworldapi/config/swagger/SwaggerConfig.java
@@ -1,22 +1,16 @@
 package click.gudrb33333.metaworldapi.config.swagger;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-@EnableOpenApi
 public class SwaggerConfig {
 
   @Value("${ext.appVersion}")
@@ -28,21 +22,14 @@ public class SwaggerConfig {
 
     return new Docket(DocumentationType.OAS_30) // open api spec 3.0
         .useDefaultResponseMessages(false)
-        .produces(getProduceContentTypes())
         .apiInfo(apiInfo())
         .groupName(version)
         .select()
         .apis(
             RequestHandlerSelectors.basePackage("click.gudrb33333.metaworldapi.api")
                 .or(RequestHandlerSelectors.basePackage("click.gudrb33333.metaworldapi.api.v1")))
-        .paths(PathSelectors.ant("/*").or(PathSelectors.ant("/api/v1/**")))
+        .paths(PathSelectors.any())
         .build();
-  }
-
-  private Set<String> getProduceContentTypes() {
-    Set<String> produces = new HashSet<>();
-    produces.add("application/json;charset=UTF-8");
-    return produces;
   }
 
   private ApiInfo apiInfo() {
