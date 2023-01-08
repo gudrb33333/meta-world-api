ad22752315e98ae34f9b31b84c7da5e4e33020ab
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-08 14:18:52 +0900

    Add ResponseStatus to AuthController.java

--- src/main/java/click/gudrb33333/metaworldapi/api/v1/auth/AuthController.java
+++ src/main/java/click/gudrb33333/metaworldapi/api/v1/auth/AuthController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
@@ -30,6 +31,7 @@ public class AuthController {
   private final AuthService authService;
 
   @PostMapping("/signup")
+  @ResponseStatus(HttpStatus.CREATED)
   @ApiOperation(value = "회원가입을 한다.")
   @ApiResponses(
       value = {
