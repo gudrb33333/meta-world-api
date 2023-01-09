188098a5e8d29612d98054da9a1501ab3f5d432b
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-09 15:39:20 +0900

    Add constantsValue test to ErrorMessageTest.java

--- src/test/java/click/gudrb33333/metaworldapi/exception/ErrorMessageTest.java
+++ src/test/java/click/gudrb33333/metaworldapi/exception/ErrorMessageTest.java
@@ -1,5 +1,8 @@
 package click.gudrb33333.metaworldapi.exception;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -8,4 +11,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class ErrorMessageTest {
 
   @InjectMocks ErrorMessage errorMessage;
+
+  @Test
+  void constantsValue() {
+    assertThat(ErrorMessage.CONFLICT_EMAIL)
+        .isEqualTo("email already exists(unique key violation).");
+    assertThat(ErrorMessage.CONFLICT_PROFILE).isEqualTo("profile already exists.");
+    assertThat(ErrorMessage.NOT_FOUND_MEMBER).isEqualTo("member not found.");
+    assertThat(ErrorMessage.NOT_FOUND_AVATAR).isEqualTo("avatar not found.");
+    assertThat(ErrorMessage.NOT_FOUND_PROFILE).isEqualTo("profile not found.");
+    assertThat(ErrorMessage.NOT_FOUND_CLOTHING).isEqualTo("clothing not found.");
+    assertThat(ErrorMessage.AWS_S3_UTIL_IO_ERROR).isEqualTo("IO error in AwsS3Util.");
+    assertThat(ErrorMessage.NOT_FOUND_SESSION).isEqualTo("session not found.");
+  }
 }
