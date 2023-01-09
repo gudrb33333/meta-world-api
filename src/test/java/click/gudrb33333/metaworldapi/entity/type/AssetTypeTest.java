2bced613cd8715a43162611ecb1edd7d6bb5759d
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-09 15:38:26 +0900

    Add assetTypeValue test to AssetTypeTest.java

--- src/test/java/click/gudrb33333/metaworldapi/entity/type/AssetTypeTest.java
+++ src/test/java/click/gudrb33333/metaworldapi/entity/type/AssetTypeTest.java
@@ -1,14 +1,21 @@
 package click.gudrb33333.metaworldapi.entity.type;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import click.gudrb33333.metaworldapi.entity.type.AssetType.Values;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class AssetTypeTest {
 
-    @Test
-    void values() {
-    }
+  @InjectMocks Values values;
+
+  @Test
+  void assetTypeValue() {
+    assertThat(Values.AVATAR).isEqualTo("AVATAR");
+    assertThat(Values.CLOTHING).isEqualTo("CLOTHING");
+  }
 }
