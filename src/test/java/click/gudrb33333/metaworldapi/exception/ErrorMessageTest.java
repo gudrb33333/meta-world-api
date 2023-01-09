b0e463587b68710b663d87bc060c1cb6c2550ec2
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-09 15:38:50 +0900

    Create ErrorMessageTest.java

---
+++ src/test/java/click/gudrb33333/metaworldapi/exception/ErrorMessageTest.java
@@ -0,0 +1,11 @@
+package click.gudrb33333.metaworldapi.exception;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ErrorMessageTest {
+
+  @InjectMocks ErrorMessage errorMessage;
+}
