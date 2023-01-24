199754634037c1051559ad31e352e05d2153387a
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-01-25 00:04:51 +0900

    Create RedisProdConfig.java

---
+++ src/main/java/click/gudrb33333/metaworldapi/config/redis/RedisProdConfig.java
@@ -0,0 +1,53 @@
+package click.gudrb33333.metaworldapi.config.redis;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Collections;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+@RequiredArgsConstructor
+@Profile("prod")
+public class RedisProdConfig {
+
+  @Value("${spring.redis.host}")
+  private String host;
+
+  @Value("${spring.redis.port}")
+  private Integer port;
+
+  private final ObjectMapper objectMapper;
+
+  @Bean
+  public RedisConnectionFactory lettuceConnectionFactory() {
+    List<String> nodes = Collections.singletonList(host + ":" + port);
+    RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration(nodes);
+    return new LettuceConnectionFactory(clusterConfiguration);
+  }
+
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate() {
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+    redisTemplate.setConnectionFactory(lettuceConnectionFactory());
+    redisTemplate.setEnableTransactionSupport(true);
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+
+    return redisTemplate;
+  }
+}
