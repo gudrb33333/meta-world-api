e71d65bf2ae8ccdfe86f953ddabe06b424b0bf86
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2023-07-03 18:01:37 +0900

    Fix whenNotSignedIn test code

--- src/test/java/click/gudrb33333/metaworldapi/api/v1/member/MemberControllerTest.java
+++ src/test/java/click/gudrb33333/metaworldapi/api/v1/member/MemberControllerTest.java
@@ -1,9 +1,5 @@
 package click.gudrb33333.metaworldapi.api.v1.member;
 
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -11,8 +7,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import click.gudrb33333.metaworldapi.entity.type.Role;
 import click.gudrb33333.metaworldapi.support.WithAuthMember;
-import javax.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,14 +20,17 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DisplayName("MemberControllerTest")
 class MemberControllerTest {
 
-  @Autowired MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
-  MockHttpSession mockHttpSession;
+  private MockHttpSession mockHttpSession;
 
   @BeforeEach
   void setSession() {
@@ -42,22 +41,21 @@ class MemberControllerTest {
   }
 
   @Nested
+  @DisplayName("로그인 한 사용자를 찾는 테스트")
   class findSigninMember {
 
     @Test
+    @DisplayName("로그인을 안 했을 때")
     public void whenNotSignedIn() throws Exception {
       mockMvc
           .perform(
               get("/api/v1/members/me").session(mockHttpSession))
-          .andExpect(status().isNotFound())
-          .andExpect(
-              result -> assertTrue(result.getResolvedException() instanceof EntityNotFoundException))
-          .andExpect(
-              result ->
-                  assertEquals("member not found.", result.getResolvedException().getMessage()));
+          .andExpect(status().isUnauthorized())
+          .andExpect(jsonPath("$.message").value("access denied."));
     }
 
     @Test
+    @DisplayName("로그인을 했을 때")
     @WithAuthMember(email = "test@test.com", role = Role.MEMBER)
     public void whenSignedIn() throws Exception {
       mockMvc
